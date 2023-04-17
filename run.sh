e1329ed4a9163f09e990483bde110be8250d40b9
Noah Cohen Kalafut <themathmaster3000@gmail.com>
2023-04-17 16:17:44 -0500

    Predictions, runs, new data

---
+++ run.sh
@@ -0,0 +1,6 @@
+jupyter nbconvert --execute --to notebook --inplace gene_classification.ipynb ;\
+jupyter nbconvert --execute --to notebook --inplace gene_classification-Copy1.ipynb ;\
+jupyter nbconvert --execute --to notebook --inplace gene_classification-Copy2.ipynb ;\
+jupyter nbconvert --execute --to notebook --inplace gene_classification-Copy3.ipynb ;\
+jupyter nbconvert --execute --to notebook --inplace gene_classification-Copy4.ipynb ;\
+jupyter nbconvert --execute --to notebook --inplace gene_classification-Copy5.ipynb ;\
