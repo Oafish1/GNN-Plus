d5a02ca42a480a96ad0bedceb08cb9856925f8ed
Noah Cohen Kalafut <themathmaster3000@gmail.com>
2023-06-28 16:42:32 -0500

    Visualizations, reorganization

--- run.sh
+++
@@ -1,6 +0,0 @@
-jupyter nbconvert --execute --to notebook --inplace gene_classification.ipynb ;\
-jupyter nbconvert --execute --to notebook --inplace gene_classification-Copy1.ipynb ;\
-jupyter nbconvert --execute --to notebook --inplace gene_classification-Copy2.ipynb ;\
-jupyter nbconvert --execute --to notebook --inplace gene_classification-Copy3.ipynb ;\
-jupyter nbconvert --execute --to notebook --inplace gene_classification-Copy4.ipynb ;\
-jupyter nbconvert --execute --to notebook --inplace gene_classification-Copy5.ipynb ;\
